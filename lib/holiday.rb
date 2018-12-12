a2660934f32a58c9326d196566e2431c9c250bdc
Chris Kakos <ckakos@gmail.com>
2018-12-12 20:48:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,38 +1,42 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
-  # {
-  #   :winter => {
-  #     :christmas => ["Lights", "Wreath"],
-  #     :new_years => ["Party Hats"]
-  #   },
-  #   :summer => {
-  #     :fourth_of_july => ["Fireworks", "BBQ"]
-  #   },
-  #   :fall => {
-  #     :thanksgiving => ["Turkey"]
-  #   },
-  #   :spring => {
-  #     :memorial_day => ["BBQ"]
-  #   }
-  # }
-  # return the second element in the 4th of July array
-  holiday_hash[:summer]
+    # given that holiday_hash looks like this:
+    # {
+    #   :winter => {
+    #     :christmas => ["Lights", "Wreath"],
+    #     :new_years => ["Party Hats"]
+    #   },
+    #   :summer => {
+    #     :fourth_of_july => ["Fireworks", "BBQ"]
+    #   },
+    #   :fall => {
+    #     :thanksgiving => ["Turkey"]
+    #   },
+    #   :spring => {
+    #     :memorial_day => ["BBQ"]
+    #   }
+    # }
+    # return the second element in the 4th of July array
+    holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
+    # add the second argument, which is a supply, to BOTH the
+    # Christmas AND the New Year's arrays
+    # 1. iterates through winter holidays 
+    # 2. adds a supply to each one
+    holiday_hash[:winter].each do |holiday, value|
+        value << supply
+    end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+    # add the second argument to the memorial day array
+    # 1. adds a supply to memorial day
+    # 2. adds a different supply to memorial day
+    holiday_hash[:spring][:memorial_day].push(supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
